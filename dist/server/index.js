a1194f7a841dbdf4136d026f9a02146673625892
ywzt-public03 <ywzt-public03@qq.com>
2026-07-27 10:26:44 +0800

    init

--- dist/server/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname === '/') url.pathname = '/index.html';
-    return env.ASSETS.fetch(new Request(url, request));
-  },
-};
